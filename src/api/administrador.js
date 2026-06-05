6af77828d11ac305a310294230612e4c38e92bb2
Gonzalo Martinez Bogado <gonzalomartinez520@gmail.com>
2026-06-05 16:47:36 -0300

    feat: alta de usuario, edicion de usuario y baja de usuario completo

--- src/api/administrador.js
+++ src/api/administrador.js
@@ -2,14 +2,29 @@ import apiClient from './apiClient';
 
 const administrador = {
 
-    // CREACIÓN DE USUARIOS   LUEGO ACTUALIZARLO CUANDO EMA HAGA PUSH.
+    // CREACIÓN DE USUARIOS OPERADOR, SUPERVISOR Y JEFE DE ESTACION
     crearUsuario: async (usuarioData) => {
-        const { data } = await apiClient.post('/usuarios/alta', usuarioData);
+        const { data } = await apiClient.post('/admin/usuarios/alta', usuarioData);
+        return data;
+    },
+
+    crearTransportista: async (usuarioData) => {
+        const { data } = await apiClient.post('/transportistas/alta', usuarioData);
         return data;
     },
 
     obtenerUsuarios: async () => {
         const { data } = await apiClient.get('/admin/usuarios');
+        return data;
+    },
+
+    editarUsuario: async (id, payload) => {
+        const { data } = await apiClient.put(`/admin/usuarios/editar/${id}`, payload);
+        return data;
+    },
+
+    darBajaUsuario: async (id) => {
+        const { data } = await apiClient.delete(`/admin/usuarios/baja/${id}`);
         return data;
     }
 };
